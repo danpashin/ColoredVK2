5eef64a35bde57d26e77c8cda3c6b02bf2a0ca06
Daniil Pashin <daniilpashin@icloud.com>
2018-03-31 12:16:28 +0300

    Синтаксические изменения

---
+++ ColoredVK/TweakFunctions.h
@@ -0,0 +1,52 @@
+//
+//  TweakFunctions.h
+//  ColoredVK2
+//
+//  Created by Даниил on 31.03.18.
+//
+
+@class NewDialogCell, VKSearchBar;
+@class VKMBrowserController, VKMTableController;
+
+void reloadPrefs(void);
+void reloadPrefsNotify(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo);
+void reloadMenuNotify(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo);
+void updateCornerRadius(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo);
+void updateNightTheme(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo);
+
+void showAlertWithMessage(NSString *message);
+
+void setBlur(UIView *bar, BOOL set, UIColor *color, UIBlurEffectStyle style);
+void setToolBar(UIToolbar *toolbar);
+void setupTranslucence(UIView *view, UIColor *backColor, BOOL remove);
+
+void setupTabbar(void);
+void resetTabBar(void);
+void setupHeaderFooterView(UITableViewHeaderFooterView *view, UITableView *tableView);
+void setupNewDialogCellFromNightTheme(NewDialogCell *dialogCell);
+
+void performInitialCellSetup(UITableViewCell *cell);
+void setupAudioPlayer(UIView *hostView, UIColor *color);
+void actionChangeCornerRadius(void);
+
+void resetUISearchBar(UISearchBar *searchBar);
+void setupUISearchBar(UISearchBar *searchBar);
+void resetNavigationBar(UINavigationBar *navBar);
+
+void uncaughtExceptionHandler(NSException *exception);
+
+void setupSearchController(UISearchDisplayController *controller, BOOL reset);
+void setupCellForSearchController(UITableViewCell *cell, UISearchDisplayController *searchController);
+void hideFastButtonForController(VKMBrowserController *browserController);
+
+void setupExtraSettingsController(VKMTableController *controller);
+void setupExtraSettingsCell(UITableViewCell *cell);
+
+void setupNightSeparatorForView(UIView *view);
+void setupNightTextField(UITextField *textField);
+
+void setupNewSearchBar(VKSearchBar *searchBar, UIColor *tintColor, UIColor *blurTone, UIBlurEffectStyle blurStyle);
+void resetNewSearchBar(VKSearchBar *searchBar);
+
+UIVisualEffectView *blurForView(UIView *view, NSInteger tag);
+NSAttributedString *attributedStringForNightTheme(NSAttributedString * text);
---
+++ ColoredVK/TweakVariables.h
@@ -0,0 +1,171 @@
+//
+//  TweakVariables.h
+//  ColoredVK2
+//
+//  Created by Даниил on 31.03.18.
+//
+
+@class ColoredVKMainController;
+
+typedef NS_ENUM(NSInteger, CVKCellSelectionStyle) {
+    CVKCellSelectionStyleNone = 0,
+    CVKCellSelectionStyleTransparent,
+    CVKCellSelectionStyleBlurred
+};
+
+extern BOOL premiumEnabled;
+extern ColoredVKMainController *cvkMainController;
+extern NSBundle *cvkBunlde;
+
+extern CVKCellSelectionStyle menuSelectionStyle;
+
+extern BOOL enableNightTheme;
+extern BOOL enabled;
+extern BOOL enabledToolBarColor;
+extern BOOL enabledTabbarColor;
+extern BOOL enabledBarColor;
+extern BOOL enabledMessagesListImage;
+extern BOOL enabledGroupsListImage;
+extern BOOL enabledAudioImage;
+extern BOOL enabledMenuImage;
+extern BOOL enabledFriendsImage;
+
+extern BOOL changeMenuTextColor;
+extern BOOL enabledSettingsExtraImage;
+extern BOOL useSettingsExtraParallax;
+extern BOOL settingsExtraUseBackgroundBlur;
+extern BOOL changeSettingsExtraTextColor;
+extern BOOL hideSettingsSeparators;
+extern BOOL showFastDownloadButton;
+extern BOOL useMenuParallax;
+extern BOOL menuUseBackgroundBlur;
+extern BOOL enabledBarImage;
+
+extern BOOL hideMenuSearch;
+extern BOOL showBar;
+extern BOOL shouldCheckUpdates;
+extern BOOL changeSBColors;
+extern BOOL hideMenuSeparators;
+extern BOOL messagesUseBlur;
+extern BOOL useCustomMessageReadColor;
+extern BOOL useCustomDialogsUnreadColor;
+extern BOOL menuUseBlur;
+extern BOOL changeMessagesInput;
+
+extern BOOL hideMessagesNavBarItems;
+extern BOOL useMessageBubbleTintColor;
+extern BOOL changeSwitchColor;
+extern BOOL showCommentSeparators;
+extern BOOL disableGroupCovers;
+extern BOOL changeAudioPlayerAppearance;
+extern BOOL enablePlayerGestures;
+extern BOOL enabledSettingsImage;
+extern BOOL hideMessagesListSeparators;
+extern BOOL hideGroupsListSeparators;
+
+extern BOOL hideAudiosSeparators;
+extern BOOL hideFriendsSeparators;
+extern BOOL hideVideosSeparators;
+extern BOOL hideSettingsExtraSeparators;
+extern BOOL messagesListUseBlur;
+extern BOOL groupsListUseBlur;
+extern BOOL audiosUseBlur;
+extern BOOL friendsUseBlur;
+extern BOOL videosUseBlur;
+extern BOOL settingsUseBlur;
+
+extern BOOL settingsExtraUseBlur;
+extern BOOL messagesListUseBackgroundBlur;
+extern BOOL groupsListUseBackgroundBlur;
+extern BOOL audiosUseBackgroundBlur;
+extern BOOL friendsUseBackgroundBlur;
+extern BOOL videosUseBackgroundBlur;
+extern BOOL settingsUseBackgroundBlur;
+extern BOOL useMessagesListParallax;
+extern BOOL useGroupsListParallax;
+extern BOOL useAudioParallax;
+
+extern BOOL useFriendsParallax;
+extern BOOL useVideosParallax;
+extern BOOL useSettingsParallax;
+extern BOOL showMenuCell;
+extern BOOL changeMessagesListTextColor;
+extern BOOL changeGroupsListTextColor;
+extern BOOL changeAudiosTextColor;
+extern BOOL changeFriendsTextColor;
+extern BOOL changeVideosTextColor;
+extern BOOL changeSettingsTextColor;
+
+extern BOOL useMessagesParallax;
+extern BOOL enabledMessagesImage;
+extern BOOL messagesUseBackgroundBlur;
+extern BOOL changeMessagesTextColor;
+extern BOOL enabledVideosImage;
+
+extern UIColor *barForegroundColor;
+extern UIColor *toolBarBackgroundColor;
+extern UIColor *toolBarForegroundColor;
+extern UIColor *messagesListBlurTone;
+extern UIColor *groupsListBlurTone;
+extern UIColor *audiosBlurTone;
+extern UIColor *friendsBlurTone;
+extern UIColor *audioPlayerTintColor;
+extern UIColor *menuTextColor;
+extern UIColor *tabbarBackgroundColor;
+
+extern UIColor *tabbarSelForegroundColor;
+extern UIColor *tabbarForegroundColor;
+extern UIColor *settingsExtraTextColor;
+extern UIColor *SBBackgroundColor;
+extern UIColor *menuSeparatorColor;
+extern UIColor *messageBubbleTintColor;
+extern UIColor *messageBubbleSentTintColor;
+extern UIColor *messagesTextColor;
+extern UIColor *messagesBlurTone;
+extern UIColor *menuBlurTone;
+
+extern UIColor *messagesInputTextColor;
+extern UIColor *messagesInputBackColor;
+extern UIColor *dialogsUnreadColor;
+extern UIColor *messageUnreadColor;
+extern UIColor *menuSeparatorColor;
+extern UIColor *barBackgroundColor;
+extern UIColor *switchesTintColor;
+extern UIColor *switchesOnTintColor;
+extern UIColor *messagesListTextColor;
+extern UIColor *groupsListTextColor;
+
+extern UIColor *audiosTextColor;
+extern UIColor *friendsTextColor;
+extern UIColor *videosTextColor;
+extern UIColor *settingsTextColor;
+extern UIColor *videosBlurTone;
+extern UIColor *settingsBlurTone;
+extern UIColor *settingsExtraBlurTone;
+extern UIColor *menuSelectionColor;
+extern UIColor *SBForegroundColor;
+
+extern CGFloat appCornerRadius;
+extern CGFloat settingsExtraImageBlackout;
+extern CGFloat menuImageBlackout;
+extern CGFloat chatListImageBlackout;
+extern CGFloat groupsListImageBlackout;
+extern CGFloat audioImageBlackout;
+extern CGFloat friendsImageBlackout;
+extern CGFloat videosImageBlackout;
+extern CGFloat settingsImageBlackout;
+extern CGFloat chatImageBlackout;
+
+extern CGFloat navbarImageBlackout;
+
+extern UIBlurEffectStyle menuBlurStyle;
+extern UIBlurEffectStyle messagesBlurStyle;
+extern UIBlurEffectStyle messagesListBlurStyle;
+extern UIBlurEffectStyle groupsListBlurStyle;
+extern UIBlurEffectStyle audiosBlurStyle;
+extern UIBlurEffectStyle friendsBlurStyle;
+extern UIBlurEffectStyle videosBlurStyle;
+extern UIBlurEffectStyle settingsBlurStyle;
+extern UIBlurEffectStyle settingsExtraBlurStyle;
+
+extern UIKeyboardAppearance keyboardStyle;
